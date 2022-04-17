050d5691152f3761bcf7e909150359b5da0bdac5
Alvaro Flores <almaflomon@gmail.com>
2022-04-17 01:55:37 -0400

    Proyecto peliculas subido

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
+   
+
+
+class HomeScreen extends StatelessWidget {
+
+  
+
+   
+  const HomeScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
+    
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Peliculas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search_outlined),
+            onPressed: () => showSearch(context: context,
+             delegate: MovieSearchDelegate()
+             ),
+            
+          )
+        ],
+        
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //tarjetas principales
+            CardSwiper(movies: moviesProvider.onDisplayMovies),
+      
+            //Listado horizontal de peliculas
+            MovieSlider(
+            movies: moviesProvider.popularMovies,
+            title: 'Populares',
+            onNextPage: () => moviesProvider.getPopularMovies(),
+            ),                
+          ],    
+        ),
+      ),
+    );
+  }
+}
